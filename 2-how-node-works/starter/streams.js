cd224e3ca0cd1be85284ab0d454082a7b8b560c9
Noah <na933950@gmail.com>
2023-09-11 00:05:30 -0700

    Learned POST request in Express

---
+++ 2-how-node-works/starter/streams.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+  // Solution 1
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+
+  // Solution 2: Streams
+  const readable = fs.createReadStream("tests-file.txt");
+//   readable.on("data", (chunk) => {
+//     res.write(chunk);
+//   });
+//   readable.on("end", () => {
+//     res.end();
+//   });
+//   readable.on("error", (err) => {
+//     console.log(err);
+//     res.statusCode = 500;
+//     res.end("File not found!");
+//   });
+    readable.pipe(res);
+    // readable source pipes the data it recieves to a writeable destination
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on port 8000");
+});
--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -15,22 +15,40 @@ app.use(express.json());
 //   res.send('You can post to this endpoint');
 // });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
 app.post('/api/v1/tours', (req, res) => {
-    console.log(req.body);
-    res.send('Done');
-})
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
