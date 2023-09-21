40848aecf9a3caee78f3699192df9094f807bde0
Noah <na933950@gmail.com>
2023-09-20 17:17:22 -0700

    Connected Mongoose to MongoDB server and tried initial document save

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,50 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(DB).then(() => {
+  console.log('DB Conneciton Successful');
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The ParkHiker',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 //Server
 const port = process.env.PORT || 3000;
