0cd47e7e81a5da376f39e9711deddbf5fe593655
Noah <na933950@gmail.com>
2023-09-23 20:45:15 -0700

    Implementing error handling and more data validation

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -11,15 +13,39 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Allows us to parse json object
 app.use(express.json());
+
+// Allows us to access documents in the file system
 app.use(express.static(`${__dirname}/public`));
+
+//Adds a requestTime to the request
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// Routers
+// Routers - original route here, the router handles additional routes on it
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//Adding a middleware here is only hit if not hit by other routers
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Sorry, that is not a valid API route: ${req.originalUrl}`,
+  // });
+
+  const err = new AppError(
+    `Can't find ${req.originalUrl} on this server!`,
+    404,
+  );
+
+  next(err);
+});
+
+// Global error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
